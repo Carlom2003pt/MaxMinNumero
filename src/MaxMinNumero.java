87b003cc531eae84b08eabb9a1e1138b36a36a34
Carlom2003pt <carlom634@gmail.com>
2022-02-09 15:26:19 +0000

    MaxMinNumero

---
+++ src/MaxMinNumero.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class MaxMinNumero {
+    public static void main(String[] args)
+    {
+        int[] colecao = new int[20];
+        Random rnd = new Random();
+        int max=0;
+        int min=999999999;
+        double soma=0;
+
+        for(int i = 0; i<colecao.length;i++)
+        {
+            colecao[i]=rnd.nextInt(1000);
+        }
+
+        for(int i =0;i<colecao.length;i++)
+        {
+            System.out.println(colecao[i]);
+            if(colecao[i]<min)
+                min=colecao[i];
+            if(colecao[i]>max)
+                max=colecao[i];
+            soma+=colecao[i];
+
+        }
+        double media=soma/colecao.length;
+
+        System.out.println("Máximo: " + max);
+        System.out.println("Minimo: " + min);
+        System.out.println("Media: " + media);
+    }
+
+}
